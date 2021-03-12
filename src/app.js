c76704a191877d56d4c2fc2bea060ffa19e16ffc
Josephine Han <jsphnhan@gmail.com>
2021-03-11 21:23:32 -0800

    edited header

--- src/app.js
+++ src/app.js
@@ -17,10 +17,7 @@ app.use(cors());
 app.use(helmet());
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://watchit-app-seven.vercel.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
