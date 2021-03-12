226c3408b70308167684f2f924cf84fde4ccf7a2
Josephine Han <jsphnhan@gmail.com>
2021-03-11 21:48:44 -0800

    headers

--- src/app.js
+++ src/app.js
@@ -18,9 +18,10 @@ app.use(helmet());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE");
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "X-Requested-With,content-type, Authorization"
   );
   next();
 });
